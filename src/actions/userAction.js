9e9ac3b92dccc943f54d6ae7c8ecffb824abbb12
itsmuhammadishaq <ishaq0600@gmail.com>
2025-10-31 13:16:44 +0500

    update login

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -21,7 +21,7 @@ export const login = (email, password) => async (dispatch) => {
       },
     };
     const { data } = await axios.post(
-      "/api/users/login",
+      `${process.env.REACT_APP_BACKEND_URL}/api/users/login`,
       {
         email,
         password,
