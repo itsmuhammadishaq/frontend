1ec4f134c459fe404182316bc862759b26158b73
itsmuhammadishaq <ishaq0600@gmail.com>
2025-11-11 11:37:58 +0500

    update

--- src/components/mainscreen.js
+++ src/components/mainscreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Container from 'react-bootstrap/esm/Container'
 import Row from 'react-bootstrap/esm/Row'
-import './mainscreen.css'
+
 
 const MainScreen = ({title,children}) => {
   return (
